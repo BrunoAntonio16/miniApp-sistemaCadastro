a99bac4c23347e82324cf3f3ab729dca547abc9c
DevBruno <brunoantonio.dev0016@gmail.com>
2024-07-05 16:36:09 -0300

    Adição da tela de home em conjunto com a tela interna - Adição do sistema de rotas e sistema de validação de formulario...

---
+++ lib/Telas/Home.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'TelaInterna.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool entrar = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:
+            (entrar) ? Colors.redAccent : const Color.fromARGB(255, 26, 72, 211),
+        title: Text(
+          (entrar) ? "Login Screen" : "Register Screen",
+          style: const TextStyle(color: Colors.black87, fontSize: 18),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor:
+          (entrar) ? Colors.black45 : const Color.fromARGB(255, 84, 86, 96),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Form(
+          key: _formKey,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+              Icon(
+                (entrar)
+                    ? Icons.account_circle_rounded
+                    : Icons.account_circle_outlined,
+                color: Colors.white,
+                size: 110,
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                validator: (String? value) {
+                  if (value == null) {
+                    return "O campo Email precisa ser preenchido...";
+                  }
+                  if (value.length < 5) {
+                    return "O campo Email precisa ter o minimo 5 caracteres";
+                  }
+                  if (!value.contains("@")) {
+                    return "O campo email precisa ter o (@)";
+                  }
+                  if (!value.contains(".")) {
+                    return "O campo email precisa ter o (.)";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    hintText: "E-mail",
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 16,
+                    )),
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                validator: (String? value) {
+                  if (value == null) {
+                    return "O campo Senha precisa ser preenchido...";
+                  }
+                  if (value.length < 8) {
+                    return "O campo Senha precisa ter o minimo 8 caracteres";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    hintText: "password",
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 16,
+                    )),
+                obscureText: true,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.w200,
+                  fontSize: 16,
+                ),
+              ),
+              Visibility(
+                  visible: !entrar,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        validator: (String? value) {
+                          if (value == null) {
+                            return "O campo Confirme a Senha precisa ser preenchido...";
+                          }
+                          if (value.length < 8) {
+                            return "O campo Confirme a Senha precisa ter o minimo 8 caracteres";
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                            hintText: "confirm password",
+                            fillColor: Colors.white,
+                            filled: true,
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w200,
+                              fontSize: 16,
+                            )),
+                        obscureText: true,
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        validator: (String? value) {
+                          if (value == null) {
+                            return "O Name precisa ser preenchido...";
+                          }
+                          if (value.length < 3) {
+                            return "O campo Name precisa ter o minimo 8 caracteres";
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                            hintText: "Name",
+                            fillColor: Colors.white,
+                            filled: true,
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w200,
+                              fontSize: 16,
+                            )),
+                      ),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 15),
+                child: SizedBox(
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      botaoEntrar();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: (entrar)
+                            ? Colors.redAccent
+                            : const Color.fromARGB(255, 26, 72, 211),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100))),
+                    child: Text(
+                      (entrar) ? "Join" : "Register",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    entrar = !entrar;
+                  });
+                },
+                child: Text(
+                  (entrar) ? "Register" : "Join",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w200,
+                    fontSize: 16,
+                  ),
+                ),
+              )
+            ])),
+      ),
+    );
+  }
+
+    botaoEntrar() {
+      if (_formKey.currentState!.validate()) {
+        Navigator.push(context,MaterialPageRoute(builder: (context) => const TelaInterna())); 
+      }else {
+        print("Não funcionou");
+      }
+    }
+}
---
+++ lib/Telas/TelaInterna.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class TelaInterna extends StatefulWidget{
+  const TelaInterna ({super.key});
+
+  @override
+  State<TelaInterna> createState() => _TelaInternaState();
+}
+
+class _TelaInternaState extends State<TelaInterna> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text(
+            "Mini App",
+        style: TextStyle(color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child:  Container(
+          height: 300,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(15),
+            border:  Border.all(
+              color: const Color.fromARGB(255, 240, 248, 255),
+              width: 4,
+            ),
+          ),
+          child:  Column(
+            children: [
+              const Padding(padding:  EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Olá, seja bem-vindo! ", style: TextStyle(color: Colors.white)),
+                    Text("Usuário", style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(padding: const EdgeInsets.fromLTRB(8,0, 8, 16),
+              child: Divider(
+                color: Colors.black,
+                ),
+              ),
+              ElevatedButton(onPressed: () {
+                Navigator.pop(context);
+              },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black54,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )
+                ),
+                child:
+                const Text("Logout",
+                  style:  TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,)
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
